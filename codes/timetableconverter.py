ed9c42d41f175d2404cac97514fb12c3b4355e29
Francis Ralph <francis341593@gmail.com>
2020-10-26 00:45:41 +0800

    rename Images to image for consistency

--- codes/timetableconverter.py
+++ codes/timetableconverter.py
@@ -22,10 +22,10 @@ class TimeTable():
         thresh,img_bin = cv2.threshold(self.img,128,255,cv2.THRESH_BINARY |cv2.THRESH_OTSU)
         #inverting the image 
         img_bin = 255-img_bin
-        cv2.imwrite("../Images/inverted.png",img_bin)
+        cv2.imwrite("../images/inverted.png", img_bin)
         #Plotting the image to see the output
         plotting = plt.imshow(img_bin,cmap='gray')
         plt.show()
 
 
-TimeTable("../Images/test.png")
+TimeTable("../images/test.png")
