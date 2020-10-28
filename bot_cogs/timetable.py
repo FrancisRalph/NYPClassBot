64ea68d4572a21883bf9022c5bde61c3f48d715e
Francis Ralph <francis341593@gmail.com>
2020-10-29 00:30:22 +0800

    add exception handling for timetable.py

--- bot_cogs/timetable.py
+++ bot_cogs/timetable.py
@@ -11,20 +11,42 @@ class TimeTable(BaseCog):
     @commands.command()
     async def upload_timetable(self, ctx: commands.Context):
         author: Union[discord.User, discord.Member] = ctx.author
-        await author.send("Please upload an image of the timetable from the NYP website")
-        await ctx.send("You have been prompted in your DMs")
+
+        try:
+            await author.send("Please upload an image of the timetable from the NYP website")
+        except Exception as e:
+            if type(e) == discord.Forbidden:
+                await ctx.send(
+                    "{} please turn on your DMs and try again."
+                        .format(author.mention)
+                )
+            else:
+                await ctx.send(
+                    "{} I was unable to send you a DM. Error: {}"
+                    .format(author.mention, e)
+                )
+            # stop function from running since DM could not be sent
+            return
+        else:
+            await ctx.send("You have been prompted in your DMs.")
 
         def check(msg: discord.Message):
-            return len(msg.attachments) > 0
+            return (
+                msg.author == author
+                and msg.channel.type == discord.ChannelType.private
+                and len(msg.attachments) > 0
+            )
 
         try:
             received_msg: discord.Message = await self.bot.wait_for("message", check=check, timeout=30)
         except asyncio.TimeoutError:
-            await ctx.send("You did not send an image, the prompt has been cancelled.")
+            await ctx.send("You did not send an image on time, the prompt has been cancelled.")
+            return
         else:
-            await ctx.send("(WIP) You sent {} images.\n{}"
-                           .format(len(received_msg.attachments), received_msg.attachments[0].url)
-                           )
+            await author.send(
+                "(WIP) You sent {} images.\n{}"
+                .format(len(received_msg.attachments), received_msg.attachments[0].url)
+            )
 
 
 def setup(bot: commands.Bot):
