446478d77dc712f66fddab449d07ae76cadc05e4
eeeyoore <67103810+eeeyoore@users.noreply.github.com>
2020-10-28 23:17:01 +0800

    added model links and comments

--- codes/upscaler.py
+++ codes/upscaler.py
@@ -6,10 +6,22 @@ start_time = time.time()
 
 sr = dnn_superres.DnnSuperResImpl_create()
 image = cv2.imread("./input/input.png")
+
+#change path to change model
 path = "./models/EDSR_x3.pb"
 sr.readModel(path)
+
+# first param is the name of the model
+# second param is the scale of the model, scale of the model is in the name of the file of the model
 sr.setModel("edsr", 3)
+
 result = sr.upsample(image)
 cv2.imwrite("./Images/upscaledx3.png", result)
 
 print("--- %s seconds ---" % (time.time() - start_time))
+
+#download models
+#esdr: https://github.com/Saafke/EDSR_Tensorflow/tree/master/models
+#lapsrn: https://github.com/Saafke/FSRCNN_Tensorflow/tree/master/models
+#espcn: https://github.com/fannymonori/TF-ESPCN/tree/master/export
+#fsrcnn: https://github.com/fannymonori/TF-LapSRN/tree/master/export
