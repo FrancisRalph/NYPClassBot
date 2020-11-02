8231b594e8a2181cd31e2a4c82ae569c51980153
eeeyoore <67103810+eeeyoore@users.noreply.github.com>
2020-11-02 08:24:57 +0800

    updated data base and process

--- codes/database.py
+++ codes/database.py
@@ -1,74 +1,53 @@
 import pymongo
 from pymongo import MongoClient
 import tabulate
+import dataprocess
 
 cluster = MongoClient(
     "mongodb+srv://NYPCLASSBOT:fatpeepee123@nyp-class-bot.boaao.mongodb.net/class-links?retryWrites=true&w=majority"
 )
 db = cluster["class-links"]
-collection = db["timing-links"]
 
 
-def insertEntry(guildId, subj, lessonType, day, time):
-    post = {
-        "guildId": guildId,
-        "subject": subj,
-        "lessonType": lessonType,
-        "day": day,
-        "time": time,
-    }
-    collection.insert_one(post)
-    print("Entry is inserted.")
-    return "Entry is inserted."
+class Db:
+    def __init__(self, guildId):
+        self.__guildId = guildId
+        self.__collection = db[self.__guildId]
 
+        def delSelf(guildId):
+            db.self.__collection.remove(guildId)
 
-# inserting multiple entry
-# post2 = {"_id": "subject", "time": 1200, "day:": "link"}
-# collection.insert_many([post, post2])
+    def insertEntry(self, subj, day, time):
+        post = {"day": day, "time": time, "subject": subj}
+        self.__collection.insert_one(post)
+        print("Entry is inserted.")
+        return "Entry is inserted."
 
+    def insertManyEntry(self, arrayOfPosts):
+        db.self.__collection.insertMany(arrayOfPosts)
+        print("Entries have been inserted.")
 
-def findEntry(guildId):
-    try:
-        results = collection.find({"guildId": guildId})
-        header = results[1].keys()
-        rows = [x.values() for x in results]
-    except IOError:
-        print("Error! Id does not exists in database.")
-        return "Error! Id does not exists in database."
-    print("Table is shown.")
-    return tabulate.tabulate(rows, header, tablefmt="rst")
+    # inserting multiple entry
+    # post2 = {"_id": "subject", "time": 1200, "day:": "link"}
+    # collection.insert_many([post, post2])
 
+    def updateEntry(self, subj, day, time, newField, newValue):
+        self.__collection.update_one(
+            {"day": day, "time": time, "subject": subj},
+            {"$set": {newField: newValue}},
+        )
+        print("Entry has been updated.")
+        return "Entry has been updated "
 
-def updateEntry(guildId, subject, day, newField, newValue):
-    print(findEntry(guildId))
-    collection.update_one(
-        {"guildId": guildId, "subject": subject, "day": day},
-        {"$set": {newField: newValue}},
-    )
-    print("Entry has been updated.")
-    return "Entry has been updated "
-
-
-def countEntry():
-    return collection.count_documents({})
-
-
-def deleteEntry(guildId, subj, lessonType, day, time):
-    # show all entries for discord server
-    findEntry(guildId)
-    # from the entry data, enter criterias for it to be deleted.
-    collection.delete_one(
-        {
-            "guildId": guildId,
-            "subject": subj,
-            "lessonType": lessonType,
-            "day": day,
-            "time": time,
-        }
-    )
-    print("Entry has been deleted.")
-    return "Entry has been deleted."
-
-
-# insertEntry('1234', 'human', 1, 1200)
-# updateEntry("123456", "poopoo", "subj", "benis")
+    def deleteSingleEntry(self, subj, day, time):
+        # show all entries for discord server
+        # from the entry data, enter criterias for it to be deleted.
+        self.__collection.delete_one(
+            {
+                "day": day,
+                "time": time,
+                "subject": subj,
+            }
+        )
+        print("Entry has been deleted.")
+        return "Entry has been deleted."
--- codes/dataprocess.py
+++ codes/dataprocess.py
@@ -1,73 +1,108 @@
+# includes cleaning the excel sheet from the convert and,
+# outputing the timetable as a string for output in discord.
+
 import pandas as pd
 import numpy as np
-import tabulate
-
-# removes any unwanted unamed columns
-df = pd.read_excel("1.xlsx", index_col=[0])
-# removes all the \n & and \x0c thingy
-df = df.replace(r"\\n", " ", regex=True)
-df = df.replace(r"\\x0c", " ", regex=True)
-
-# removes whatever that is not digits in the time column
-df[0] = df[0].str.replace(r"\D", "", regex=True)
-# words to remove
-delWords = [
-    "WEEK",
-    "EEK",
-    "MONDAY",
-    "TUESDAY",
-    "WEDNESDAY",
-    "THURSDAY",
-    "FRIDAY",
-    "SATURDAY",
-    "1-17",
-]
-
-lessonTypes = ["LEC", "LAB", "TUT"]
-
-# creates new row on top of the xl sheet with headers
-new_row = pd.DataFrame(
-    {0: "TIME", 1: "MON", 2: "TUES", 3: "WED", 4: "THURS", 5: "FRI", 6: "SAT"},
-    index=[1],
-)
-df = pd.concat([new_row, df]).reset_index(drop=True)
-
-# this code looks like shit, but it works
-# formats the timing
-def formatTime():
-    for x in range(1, len(df[0])):
-        df[0][x] = df[0][x][:4] + "-" + df[0][x][4:]
-    print("Format completed.")
-    # print(df[0])
+from tabulate import tabulate
 
 
-# random large number for it to iterrate
-def replaceCommon():
-    for x in range(100):
-        try:
-            for char in delWords:
-                df[x] = df[x].str.replace(char, "")
-        except KeyError:
-            print("Replacing completed.")
-            break
-    # print(df[1])
+def cleanExcel():
+    # data precleaning ======================================
+    # removes any unwanted unamed columns
+    df = pd.read_excel("1.xlsx", index_col=[0])
+    # removes all the \n & and \x0c thingies
+    df = df.replace(r"\\n", " ", regex=True)
+    df = df.replace(r"\\x0c", " ", regex=True)
+    # removes whatever that is not digits in the time column
+    df[0] = df[0].str.replace(r"\D", "", regex=True)
+    # =======================================================
+    new_row = pd.DataFrame(
+        {0: "TIME", 1: "MON", 2: "TUES", 3: "WED", 4: "THURS", 5: "FRI", 6: "SAT"},
+        index=[1],
+    )
+    days_to_number = {"MON": 0, "TUES": 1, "WED": 2, "THURS": 3, "FRI": 4, "SAT": 5}
+    df = pd.concat([new_row, df]).reset_index(drop=True)
+    # list is pretty limited, gonna need more keywords ://
+    # words to remove =====================
+    delWords = [
+        "WEEK",
+        "EEK",
+        "MONDAY",
+        "TUESDAY",
+        "WEDNESDAY",
+        "THURSDAY",
+        "FRIDAY",
+        "SATURDAY",
+        "1-17",
+    ]
+    lessonTypes = ["LEC", "LAB", "TUT"]
+    # =====================================
 
+    # creates new row on top of the xl sheet with headers
 
-def removeBeforeLessonTypes():
-    for types in lessonTypes:
-        for colNo in range(len(df.columns)):
-            for rowNo in range(len(df)):
-                try:
+    # this code looks like shit, but it works
+    # formats the timing
+    def formatTime():
+        for x in range(1, len(df[0])):
+            df[0][x] = df[0][x][:4]
+        print("Format completed.")
+        # print(df[0])
 
-                    index = df[colNo][rowNo].index(types)
-                    df[colNo][rowNo] = df[colNo][rowNo][index:]
+    # random large number for it to iterrate
+    # gonna add more keywords
+    def replaceCommon():
+        for x in range(100):
+            try:
+                for char in delWords:
+                    df[x] = df[x].str.replace(char, "")
+            except KeyError:
+                print("Replacing completed.")
+                break
+        # print(df[1])
 
-                except:
-                    pass
-    print("Removed completed.")
+    def removeStrBeforeLessonTypes():
+        for types in lessonTypes:
+            for colNo in range(len(df.columns)):
+                for rowNo in range(len(df)):
+                    try:
+                        # going thru each individual cell and replacing shit,
+                        # probably easier way to do this but im too lazy.
+                        index = df[colNo][rowNo].index(types)
+                        df[colNo][rowNo] = df[colNo][rowNo][index:]
 
+                    except:
+                        pass
+        print("Removed completed.")
 
-formatTime()
-replaceCommon()
-removeBeforeLessonTypes()
-df.to_csv("newShit.csv")
+    def collectEntry():
+        entryForClass = []
+        for colNo in range(1, len(df.columns)):
+            for rowNo in range(1, len(df)):
+                post = {"day": "", "time": "", "subject": ""}
+                post["day"] = days_to_number[df[colNo][0]]
+                post["time"] = df[0][rowNo]
+                post["subject"] = df[colNo][rowNo].strip()
+                if post["subject"] == "":
+                    continue
+                else:
+                    entryForClass.append(post)
+        print("Entries have been recorded.")
+        return entryForClass
+
+    # removes the Sat column if its empty
+    # cant put this a function, itll break
+    lastCol = df.columns[-1]
+    x = df[lastCol][1:].sum()
+    print(x)
+    if x.strip() == "":
+        df = df[df.columns[:-1]]
+
+    # output to csv, please include path
+    # added this into a giant function cos the code needs to be ran sequentially
+    # df.to_excel(path of file)
+    # df.to_excel("newshit.xlsx")
+    formatTime()
+    replaceCommon()
+    removeStrBeforeLessonTypes()
+    print(tabulate(df))
+    return collectEntry()
